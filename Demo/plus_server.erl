c6ee6c99c566b7fd297d83c99dc4af88d7584784
Jia Wang <wj2016@gmail.com>
2015-06-06 23:32:02 +0200

    [gen_server] demo call/cast/info

--- Demo/plus_server.erl
+++ Demo/plus_server.erl
@@ -2,7 +2,11 @@
 
 -behaviour(gen_server).
 
--export([plus/2]).
+-export([ plus/2
+        , plus_cast/2
+        , plus_info/2
+        ]).
+
 -export([start/1]).
 
 -export([start/0, start_link/0]).
@@ -16,6 +20,12 @@
 
 plus(A, B) ->
     gen_server:call(?SERVER, {plus, A, B}).
+
+plus_cast(A, B) ->
+    gen_server:cast(?SERVER, {plus_cast, A, B}).
+
+plus_info(A, B) ->
+    ?SERVER ! {plus_info, A, B}.
 
 start() ->
     start(0).
@@ -31,14 +41,19 @@ init(Opts) ->
     Extra = proplists:get_value(magic_extra, Opts, 0),
     {ok, #state{magic_extra = Extra}}.
 
-handle_call({plus, A, B}, _From, #state{magic_extra = Extra} = State) ->
+handle_call({plus, A, B} = Msg, _From, #state{magic_extra = Extra} = State) ->
+    io:format("handle call: ~p~n", [Msg]),
     Reply = {ok, A + B + Extra},
     {reply, Reply, State}.
 
-handle_cast(_Msg, State) ->
+handle_cast({plus_cast, A, B} = Msg, State) ->
+    io:format("handle cast: ~p~n", [Msg]),
+    io:format("A+B = ~p~n", [A+B]),
     {noreply, State}.
 
-handle_info(_Info, State) ->
+handle_info({plus_info, A, B} = Msg, State) ->
+    io:format("handle info: ~p~n", [Msg]),
+    io:format("A+B = ~p~n", [A+B]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
